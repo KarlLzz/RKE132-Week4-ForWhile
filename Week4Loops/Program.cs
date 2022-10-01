38f334b13b21331f464fde7cd434f1f7c49f580e
Karl Aru <karlaru@Karls-MBP-2.lan>
2022-10-01 22:02:28 +0300

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿//FOR-LOOP
+//DRY-princible - Do not repeat ypurself
+int i = 0;
+for(i = 0; i < 5; i++) //i ++ == i + 1
+{
+    Console.WriteLine("We don't need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day! ");
+
+//Console.WriteLine("We don't need no education");
